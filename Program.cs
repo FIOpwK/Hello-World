aaa95c6b7a32906ad7c4500f16e76b588444aa0c
fiopwk <dajuan-mcdonald@lambdastudents.com>
2022-03-27 19:12:31 -0400

    dotnet added Humanizer package

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-var text = "Hello World!";
+﻿using Humanizer;
+
+var text = "Quantities:";
 Console.WriteLine(text);
+
+
+//get the length of text
+System.Console.WriteLine(text.Length);
+
+static void HumaizeQuantities()
+{
+    System.Console.WriteLine("cases:".ToQuantity(0));
+    System.Console.WriteLine("case:".ToQuantity(1));
+    System.Console.WriteLine("cases:".ToQuantity(5));
+}
+
+HumaizeQuantities();
